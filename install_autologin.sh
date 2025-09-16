95e1355607eff96c655fa2faf378f732a9973612
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2025-09-16 12:48:10 +0400

    Update install_autologin.sh

--- install_autologin.sh
+++ install_autologin.sh
@@ -4,6 +4,6 @@ echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec Hyprland" >> ~/.bash_profile
 cat ~/.bash_profile
 
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin pc --noclear %I \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin <Your-Username-Here> --noclear %I \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
 sudo systemctl enable getty@tty1
