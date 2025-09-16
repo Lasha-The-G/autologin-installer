09e1a36f77ba8ef21a1e4ec1c89877972f84fcad
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2025-09-16 12:46:29 +0400

    Add files via upload

---
+++ install_autologin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec Hyprland" >> ~/.bash_profile
+cat ~/.bash_profile
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin pc --noclear %I \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
+
+sudo systemctl enable getty@tty1
