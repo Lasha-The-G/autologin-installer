4f823ee882695d21866209706de813abbe4fba5e
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2026-01-21 13:32:52 +0400

    Small fix

--- install_autologin.sh
+++ install_autologin.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec Hyprland" >> ~/.bash_profile
+if [[ $# -eq 0 ]] then
+	echo "Pass username argument"
+	exit
+else
+	echo "Registering autologin for '$1'"
+fi
+
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && exec start-hyprland" >> ~/.bash_profile
 cat ~/.bash_profile
 
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin <Your-Username-Here> --noclear %I \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $1 --noclear %I \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
 sudo systemctl enable getty@tty1
